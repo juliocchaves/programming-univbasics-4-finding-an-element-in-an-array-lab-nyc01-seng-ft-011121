588a1448a172ffe78fcdaa556cc46bf175573c55
juliochavesnyc <juliochavesnyc@gmail.com>
2020-12-01 22:14:03 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,8 +2,12 @@ def find_element_index(array, value_to_find)
   array = [1, 2, 3, 4]
   count = 0 
   value_to_find = 2
+  found_value_index = nil 
   while count < array.length do
-    puts array[count] == value_to_find
+    if array[count] == value_to_find
+      found_value_index = count
+    end
     count += 1 
   end
+  found_value_index
 end
