ba8e876bb6b04c7767bd632b31634d4f291c8c22
juliochavesnyc <juliochavesnyc@gmail.com>
2020-12-01 22:03:54 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   array = [1, 2, 3, 4]
   count = 0 
-  value_to_find = 2
+  value_to_find = 5
   while count < array.length do
     value_to_find == array
     count += 1 
